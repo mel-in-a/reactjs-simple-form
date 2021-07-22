a97a9eaee7aa4f4743794a311388bdf1f4a27c74
Mélina <purple.melinaa@gmail.com>
2021-07-22 17:16:41 +0200

    Tout fonctionne ! Youpi

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
   const [showForm, setShowForm] = useState(true);
-
-  const[result, setResult] = useState("");
+  const [result, setResult] = useState(false);
 
   const handleSubmit = (event) => {
     // annuler le comportement par défaut d'un formulaire à sa soumission
@@ -25,83 +24,91 @@ function App() {
     event.preventDefault();
 
     // eslint-disable-next-line no-unused-expressions
-    password !== password2 ? setResult(<div className="alert">Les mots de passe doivent être identiques</div>) : setShowForm = false;
+    password !== password2 ? setResult(true) : setShowForm(false);
   };
+
+  const validated = () => {
+    setShowForm(true);
+    setResult(false);
+  }
 
   return (
     <div>
       <Header />
       <div className="container">
         <div className="mini-container">
-          <h2>Create account</h2>
+          <h2 className={showForm ? 'form visible': 'form hidden'}>Create account</h2>
 
-          <div className="form" style={showForm ? "visibility:visible": "visibility:hidden"}>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              value={name}
-              placeholder="Name"
-              onChange={(event) => {
-                setName(event.target.value);
-              }}
-              required
-            />
-            <div className="spacer"></div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              value={email}
-              placeholder="Email"
-              onChange={(event) => {
-                setEmail(event.target.value);
-              }}
-              required
-            />
-            <div className="spacer"></div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              value={password}
-              required
-              placeholder="Password"
-              onChange={(event) => {
-                setPassword(event.target.value);
-              }}
-            />
-            <div className="spacer"></div>
-            <label htmlFor="password2">Confirm your password</label>
-            <input
-              type="password"
-              name="password2"
-              value={password2}
-              placeholder="Password confirmation"
-              onChange={(event) => {
-                setPassword2(event.target.value);
-              }}
-              required
-            />
-            <div className="spacer"></div>
-            {result}
-            <button type="submit" className="submit">
-              Register
-            </button>
-           
-          </form>
-          </div> 
+          <div className={showForm ? 'form visible': 'form hidden'}>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                value={name}
+                placeholder="Name"
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+                required
+              />
+              <div className="spacer"></div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Email"
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+                required
+              />
+              <div className="spacer"></div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                required
+                placeholder="Password"
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+              />
+              <div className="spacer"></div>
+              <label htmlFor="password2">Confirm your password</label>
+              <input
+                type="password"
+                name="password2"
+                value={password2}
+                placeholder="Password confirmation"
+                onChange={(event) => {
+                  setPassword2(event.target.value);
+                }}
+                required
+              />
+              <div className="spacer"></div>
+
+              <div className={result ? 'alert visible': 'alert hidden'}>
+                Les mots de passe doivent être identiques
+              </div>
+
+              <button type="submit" className="submit">
+                Register
+              </button>
+            </form>
+          </div>
           {/* end form */}
-        <div className="results">
-          <h2>Results</h2>
-          <div className="box">
+
+          <div className={showForm ? 'results hidden': 'results visible'}>
+            <h2>Results</h2>
+            <div className="box">
               <p>Name : {name}</p>
               <p>Email : {email}</p>
               <p>Password : {password}</p>
-            
+            </div>
+            <button className="edit" onClick={() =>validated()} >Edit your information</button>
           </div>
-          <button className="edit">Edit your information</button>
-        </div>
         </div>
       </div>
       <Footer />
